be4d40447593bf4801aad0e0651ddcd5766fef22
ojhama_glb <mahavir.cse@gmail.com>
2022-11-30 21:53:08 +0530

    Added war pacakgin to the application

--- src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -2,6 +2,8 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // Below annotation ( @SpringBootApplication ), is internally using many annotation. Below are the main ones :
 // @SpringBootConfiguration
@@ -10,19 +12,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @EnableAutoConfiguration
 
 @SpringBootApplication
-public class SmartcontactmanagerApplication {
+public class SmartcontactmanagerApplication extends SpringBootServletInitializer {
 
-	// This is the main starting point of a spring boot application, execution starts by calling this SpringApplication.run() method.
+	// extended the class SpringBootServletInitializer
+	// and overriden configure method to make the application work as a war
+	// deployable.
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SmartcontactmanagerApplication.class);
+	}
+
+	// This is the main starting point of a spring boot application, execution
+	// starts by calling this SpringApplication.run() method.
 	// The run method does following operations :
-	// 1. Identifies the type of application ( Web application, desktop application etc )
-	// 2. Based on the type of application, it instantiates the required context classes.
+	// 1. Identifies the type of application ( Web application, desktop application
+	// etc )
+	// 2. Based on the type of application, it instantiates the required context
+	// classes.
 	// 3. Calls the constructors of the context classes.
-	// 4. Scans the classpath for the Beans which are having @Cofiguration annotation and it registers them with the context.
+	// 4. Scans the classpath for the Beans which are having @Cofiguration
+	// annotation and it registers them with the context.
 	// 5. Inside those beans if there is any Object which is Autowired,
-	//    it will create object of them inside JVM ( specifically inside IOC container )
+	// it will create object of them inside JVM ( specifically inside IOC container
+	// )
 	// 6. Starts the embedded tomcat and registers the context with it.
 	//
-	// Note : Iside Context classes there is a functionality which enable all the features for the dependencies mentioned in POM.xml
+	// Note : Iside Context classes there is a functionality which enable all the
+	// features for the dependencies mentioned in POM.xml
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcontactmanagerApplication.class, args);
 	}
